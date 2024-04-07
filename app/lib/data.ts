56ab2f432c5d83ff929530dc3ea1eb41ec3594c0
Yanqin Qualmann <yq.qualmann@gmail.com>
2024-04-07 21:34:59 +0200

    handling error

--- app/lib/data.ts
+++ app/lib/data.ts
@@ -11,6 +11,7 @@ import {
 import { formatCurrency } from './utils';
 import { unstable_noStore as noStore } from 'next/cache';
 
+
 export async function fetchRevenue() {
   // Add noStore() here to prevent the response from being cached.
   // This is equivalent to in fetch(..., {cache: 'no-store'}).
@@ -152,7 +153,7 @@ export async function fetchInvoicesPages(query: string) {
 }
 
 export async function fetchInvoiceById(id: string) {
-  noStore();
+  noStore(); // Add noStore() here to prevent the response from being cached.
   try {
     const data = await sql<InvoiceForm>`
       SELECT
@@ -169,7 +170,7 @@ export async function fetchInvoiceById(id: string) {
       // Convert amount from cents to dollars
       amount: invoice.amount / 100,
     }));
-
+    // console.log(invoice); // Invoice is an empty array []
     return invoice[0];
   } catch (error) {
     console.error('Database Error:', error);
