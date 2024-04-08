244e14b424837af3253e9646adaffa158a68455a
Yanqin Qualmann <yq.qualmann@gmail.com>
2024-04-08 11:19:18 +0200

    Update edit-form.tsx

--- app/ui/invoices/edit-form.tsx
+++ app/ui/invoices/edit-form.tsx
@@ -41,6 +41,7 @@ export default function EditInvoiceForm({
               name="customerId"
               className="peer block w-full cursor-pointer rounded-md border border-gray-200 py-2 pl-10 text-sm outline-2 placeholder:text-gray-500"
               defaultValue={invoice.customer_id}
+              
             >
               <option value="" disabled>
                 Select a customer
@@ -53,6 +54,7 @@ export default function EditInvoiceForm({
             </select>
             <UserCircleIcon className="pointer-events-none absolute left-3 top-1/2 h-[18px] w-[18px] -translate-y-1/2 text-gray-500" />
           </div>
+         
         </div>
 
         {/* Invoice Amount */}
@@ -70,9 +72,18 @@ export default function EditInvoiceForm({
                 defaultValue={invoice.amount}
                 placeholder="Enter USD amount"
                 className="peer block w-full rounded-md border border-gray-200 py-2 pl-10 text-sm outline-2 placeholder:text-gray-500"
-              />
+                aria-describedby="amount-error"
+             />
               <CurrencyDollarIcon className="pointer-events-none absolute left-3 top-1/2 h-[18px] w-[18px] -translate-y-1/2 text-gray-500 peer-focus:text-gray-900" />
             </div>
+            <div id="amount-error" aria-live="polite" aria-atomic="true">
+                {state.errors?.amount &&
+                  state.errors.amount.map((error: string) => (
+                    <p className="mt-2 text-sm text-red-500" key={error}>
+                      {error}
+                    </p>
+                  ))}
+              </div>
           </div>
         </div>
 
@@ -107,7 +118,8 @@ export default function EditInvoiceForm({
                   value="paid"
                   defaultChecked={invoice.status === 'paid'}
                   className="h-4 w-4 cursor-pointer border-gray-300 bg-gray-100 text-gray-600 focus:ring-2"
-                />
+               
+               />
                 <label
                   htmlFor="paid"
                   className="ml-2 flex cursor-pointer items-center gap-1.5 rounded-full bg-green-500 px-3 py-1.5 text-xs font-medium text-white"
@@ -118,6 +130,14 @@ export default function EditInvoiceForm({
             </div>
           </div>
         </fieldset>
+        <div id="amount-error" aria-live="polite" aria-atomic="true">
+        {state.message &&    
+          <p className="mt-2 text-sm text-red-500">
+            {state.message}
+          </p>
+        }
+          
+      </div>
       </div>
       <div className="mt-6 flex justify-end gap-4">
         <Link
