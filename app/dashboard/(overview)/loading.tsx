81e5e54564fc79c3bea89695236ca452a384a888
Yanqin Qualmann <yq.qualmann@gmail.com>
2024-04-17 12:30:42 +0200

    review

--- app/dashboard/(overview)/loading.tsx
+++ app/dashboard/(overview)/loading.tsx
@@ -1,6 +1,8 @@
 import DashboardSkeleton from '@/app/ui/skeletons';
  
 
+//two ways you implement streaming in Next.js: 1. At the page level, with the loading.tsx file. 2. At the component level, with the Suspense component.
+
 export default function Loading() {
     return <DashboardSkeleton />;
   }
